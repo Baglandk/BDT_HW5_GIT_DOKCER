36fd410274119f8768712a85d54e758f4c4b7615
baga <www.baglan888@gmail.com>
2024-10-23 22:58:34 +0500

    first commit

--- src/myproject/the_project.py
+++ src/myproject/the_project.py
@@ -1,2 +1,39 @@
-def add_num(a, b):
-    return a + b
+import argparse
+import random
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def generate_primes(count, seed):
+    random.seed(seed)
+    primes = []
+    num = 2
+    while len(primes) < count:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Генерация простых чисел.")
+    parser.add_argument("count", type=int, help="Количество простых чисел для генерации")
+    parser.add_argument("seed", type=int, help="Seed для генератора случайных чисел")
+
+    args = parser.parse_args()
+
+    primes = generate_primes(args.count, args.seed)
+
+    for prime in primes:
+        print(prime)
+
+
+if __name__ == "__main__":
+    main()
--- tests/testMyProject.py
+++ tests/testMyProject.py
@@ -1,9 +1,12 @@
-from src.myproject.the_project import add_num
+from src.myproject.the_project import is_prime
 
 
-def test_add_num():
-    assert add_num(1, 3) == 4
+def test_is_prime():
+    assert is_prime(0) == 0
+    assert is_prime(1) == 0
+    assert is_prime(2) == 1
+    assert is_prime(17) == 1
 
 
 if __name__ == "__main__":
-    test_add_num()
+    test_is_prime()
